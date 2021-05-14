5b5af8d4b25a4387a8e6c51a904e37f50ab17365
Chirag <chiragvalecha97@gmail.com>
2021-05-14 18:49:07 +0530

    add action creators

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 
 import { data } from "../data";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount() {
@@ -13,10 +14,7 @@ class App extends React.Component {
     store.subscribe(() => {
       this.forceUpdate();
     });
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
   }
   render() {
     const movies = this.props.store.getState();
