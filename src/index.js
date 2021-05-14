ff26b438096a234fdacf3e7ab55e22a252506c6d
Chirag <chiragvalecha97@gmail.com>
2021-05-14 18:24:36 +0530

    add reducer and create a store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
 import App from "./components/App";
 import "./index.css";
+import movies from "./reducers";
+
+const store = createStore(movies);
 
 ReactDOM.render(<App />, document.getElementById("root"));
