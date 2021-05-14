cd45a8dd74a38e76802e590f2a89a6c52ca1eef6
Chirag <chiragvalecha97@gmail.com>
2021-05-14 18:40:40 +0530

    subscribe to the store with forcefulUpdate()

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,38 @@ import MovieCard from "./MovieCard";
 
 import { data } from "../data";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
-        <div className="list">
-          {data.map((movie, index) => (
-            <MovieCard movie={movie} key={`movies-${index}`} />
-          ))}
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    //api call
+    //dispatch function
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data,
+    });
+  }
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import movies from "./reducers";
 
 const store = createStore(movies);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
